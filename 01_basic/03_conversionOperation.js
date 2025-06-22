236103777c2ecbf0c7f22fceff5f78abd9b86383
vishal yadav <138583966+vishal1230@users.noreply.github.com>
2025-06-22 11:40:38 +0000

    conversion of datatype

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "45"
+console.log(typeof score);
+
+let valueInNumber= Number(score)
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+//"45" -> 45
+//"45abc" ->NaN
+//true -> 1, false-> 0
+
+let isLoggedIn=1;
+let booleanisLoggedIn= Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+console.log(typeof booleanisLoggedIn);
+
+// 1->true, 0->false
+// "" -> empty string to false
+// "vishal"-> string to true
+
+let someNumber=67
+let StringsomeNumber=String(someNumber)
+console.log(StringsomeNumber);
+console.log(typeof StringsomeNumber);
+
