7808437045b1553558449e0148d07646425c2e17
vishal yadav <138583966+vishal1230@users.noreply.github.com>
2025-06-22 11:28:59 +0000

    datatypes and variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,17 @@
+const accountID=2314
+let accountEmail="vishal@99"
+var accountPassword="12313"
+accountCity="prayag"
+let accountState ;
+console.table([accountID,accountEmail,accountPassword,accountCity])
+
+/*
+we use const to declare those variable whoes value cannot change further.
+
+we use console.table to print multiple variable at same time inside a table.
+
+we prefer to use let instead of var because
+of issue in block scope and functional scope.
+
+if we define a variable without assing a value to it , it gives us undefined as its value.
+*/
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict" // treat whole js code according to newer version guidelines
+
+// alert(3+3)   we are using nodejs not browser
+
+// code readiblity must be high 
+
+/*
+number 
+string -> "vishal"
+boolean -> true,false
+bigint
+null -> standalone value
+undefined
+symbol-> it is used to show uniqness among different components
+*/
+
+// object 
+
+// null is an object
+
+console.log( typeof "vishal")
+
+console.log( typeof undefined) // output is undefined
+
+console.log( typeof null) // output is object
+
+//typeof is an operator used to know the type of variable
