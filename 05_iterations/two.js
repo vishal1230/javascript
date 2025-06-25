9cb0c3ef809cf5ee7296f11d265b72d0492fb1b8
vishal yadav <138583966+vishal1230@users.noreply.github.com>
2025-06-25 08:41:41 +0000

    while and do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,14 @@
+// in while condition is checkd first before running the loop
+let day=1
+while(day<=7){
+    console.log(`today day is ${day}`)
+    day++
+}
+
+
+// in do while loop runs once then condition is checked
+let score=1
+do{
+    console.log(` curretn score is ${score}`)
+    score++
+}while(score<=10)
