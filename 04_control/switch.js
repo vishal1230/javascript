e7b19821b4e09f40cca32c313d639d7acc562283
vishal yadav <138583966+vishal1230@users.noreply.github.com>
2025-06-25 08:04:57 +0000

    switch in js

---
+++ 04_control/switch.js
@@ -0,0 +1,16 @@
+const month=3
+
+switch(month){ // we can pass string as key also in switch cases
+    case 1:
+        console.log("month is jan")
+        break;
+    case 2:
+        console.log("month is feb")
+        break;
+    case 3:
+        console.log("month is march")
+        break;
+    default:
+        console.log("default case executed")
+        break;        
+}
